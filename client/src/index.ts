f54f33b342796fc9d0c4e82f17b29e3982897122
baudet_c <baudet_c@etna-alternance.net>
2022-04-04 21:13:31 +0200

    server

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,33 +1,45 @@
-import process from "process"
-import { io } from "socket.io-client";
+import process from "process";
+import { io, Socket } from "socket.io-client";
 
-import {Config} from './config'
+import { Config } from './config'
 
 function LoadConfig() {
     const args = process.argv.slice(2);
 
     args.forEach((argument, index) => {
-        if(!argument.startsWith('-'))
+        if (!argument.startsWith('-'))
             return;
         switch (argument) {
             case '-h':
-                if(Number(args[index + 1]) != NaN)
-                    Config.setPort(Number(args[index+1]));
+                if (Number(args[index + 1]) != NaN)
+                    Config.setPort(Number(args[index + 1]));
                 else console.log(`Invalid port number: ${args[index + 1]}`)
-            break;
+                break;
             case '-p':
                 Config.setHostname(args[index + 1]);
-            break;
+                break;
             default:
-              console.log('Invalid argument ' + argument);
-          }
+                console.log('Invalid argument ' + argument);
+        }
     })
 
     Config.printConfig();
 }
 
+function connect() {
+
+    let url = `http://${Config.hostName}:${Config.portNumber}`
+    let socket = io(url);
+
+    if (!socket.connected) {
+        console.log(`Unable to connect to ${url}`);
+        return false;
+    }
+}
+
 function main() {
     LoadConfig();
+    connect();
 }
 
 main()
