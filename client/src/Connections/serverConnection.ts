5bf61ab9f04179f17c46a7e7b0de025b4bc98895
Hubert-Henri GRASSET-DUVIVIER <grasse_h@etna-alternance.net>
2022-04-05 09:02:20 +0200

    DS Diver Update

--- client/src/Connections/serverConnection.ts
+++ client/src/Connections/serverConnection.ts
@@ -1,21 +1,46 @@
 import {io, Socket} from 'socket.io-client'
 
 import Config from '../config'
+import DisplayDriver from '../Display/displayDriver';
 
 export default class ServerConnection {
     private socket: Socket | null = null;
+    private connectionTryCount: number = 1;
 
     ServerConnection() {
         this.socket = null;
     }
 
-    connect() {
+    async tryConnect() : Promise<boolean> {
         let url = `http://${Config.hostName}:${Config.portNumber}`
-        this.socket = io(url);
 
+        let connectionStatus = await this.connect(url);
+
+        if (connectionStatus)
+            return true;
+        
+            let tryCount = this.connectionTryCount;
+        
+        while (!connectionStatus && tryCount != 0) {
+            for(let i = 5 ; i > 0 ; i--) {
+                DisplayDriver.printOnLine(`Retrying in ${i} seconds ...`, 1);
+                await new Promise(resolve => setTimeout(resolve, 1000));
+            }
+            DisplayDriver.print('\n');
+
+            connectionStatus = await this.connect(url);
+            tryCount--;
+        }
+
+        return connectionStatus;
+    }
+
+    async connect(url: string) : Promise<boolean> {
+        this.socket = io(url);
         if(!this.socket.connected) {
-            console.log(`Unable to connect to ${url}`);
+            DisplayDriver.print(`Unable to connect to ${url}\n`);
             return false;
         }
+        return true;
     }
 }
--- client/src/Display/displayDriver.ts
+++ client/src/Display/displayDriver.ts
@@ -1,4 +1,31 @@
+import readline from 'readline'
+
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> ',
+    //terminal: true
+});
+
 export default class DisplayDriver {
 
-    
+    static chat(msg: string) {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        console.log(msg);
+        rl.prompt(true);
+    }
+
+    static printOnLine(msg: string, line: number = 1) {
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        //process.stdout.clearLine(0);
+        process.stdout.write(msg);
+        //process.stdout.moveCursor(msg.length, 0);
+    }
+
+    static print(msg: string) : void {
+        process.stdout.write(msg);
+   }
 }
