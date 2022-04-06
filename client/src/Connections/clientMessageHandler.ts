6759bd0df58b6a7e6ec4d184f596de14b5221d7b
Nathan Debaz <nathandebaz266@gmail.com>
2022-04-06 11:29:32 +0200

    consolePhase

--- client/src/Connections/clientMessageHandler.ts
+++ client/src/Connections/clientMessageHandler.ts
@@ -1,31 +1,31 @@
 import DisplayDriver from "../Display/displayDriver";
-import {ServerToClientEvents, ClientToServerEvents} from './socketEvents'
-import {io, Socket} from 'socket.io-client'
+import {ServerToClientEvents, ClientToServerEvents} from './socketEvents';
+import {io, Socket} from 'socket.io-client';
 
 export class ClientMessageHandler {
     private socket: Socket<ServerToClientEvents, ClientToServerEvents>;
 
     constructor(socket: Socket<ServerToClientEvents, ClientToServerEvents>) {
         this.socket = socket;
-    }
+    };
 
     init() {
         // let rlDriver = DisplayDriver.getDriver();
         // rlDriver.on('line', (input: string) => {
         //     return this.parseMessage(input);
         // });
-    }
+    };
 
     parseMessage(message: string) {
         if(message.startsWith('/'))
             return this.parseCommand(message);
         else
             return this.sendMessage(message);
-    }
+    };
 
     checkArgc(args: string[], count: number) {
         return (args.length == count);
-    }
+    };
 
     parseCommand(command: string) {
         let commandArgs = command.split(' ', 2);
@@ -49,7 +49,7 @@ export class ClientMessageHandler {
             case '/leave':
                 this.sendLeaveRequest();
                 break;
-                
+
             case '/create':
                 if(this.checkArgc(commandArgs, 2))
                     this.sendCreateRoomRequest(commandArgs[1]);
@@ -59,7 +59,7 @@ export class ClientMessageHandler {
             case '/users':
                 this.sendUsersListRequest();
                 break;
-                
+
             case '/add':
                 if(this.checkArgc(commandArgs, 2))
                     this.sendAddFriendRequest(commandArgs[1]);
@@ -76,61 +76,61 @@ export class ClientMessageHandler {
             default:
                 DisplayDriver.print(`Invalid command ${commandArgs[0]} \n`);
                 break;
-                
-        }
-    }
+
+        };
+    };
 
     sendRoomsListRequest() {
         this.socket.emit('listRoom');
         return;
-    }
+    };
 
     sendJoinRequest(roomName: string = '') {
         if(roomName == '') {
             DisplayDriver.print('Invalid command /join \n');
             DisplayDriver.print('Usage: /join {room_name}\n');
             return;
-        }
+        };
 
         this.socket.emit('joinRoom', roomName);
         return;
-    }
+    };
 
     sendHistoryRequest(messageCount: number = 0) {
         if(messageCount == NaN) {
             DisplayDriver.print('Invalid command /history \n');
             DisplayDriver.print('Usage: /history {message_count}');
             return;
-        }
+        };
 
         if(messageCount == 0) {
             messageCount = 20;
-        }
+        };
 
         this.socket.emit('history', messageCount);
         return;
-    }
+    };
 
     sendLeaveRequest() {
         this.socket.emit('leaveRoom');
         return;
-    }
+    };
 
     sendCreateRoomRequest(roomName: string = '') {
         if(roomName == '') {
             DisplayDriver.print('Invalid command /create \n');
             DisplayDriver.print('Usage: /create {room_name}\n');
             return;
-        }
+        };
 
         this.socket.emit('addRoom', roomName);
         return;
-    }
+    };
 
     sendUsersListRequest() {
         this.socket.emit('listUser');
         return;
-    }
+    };
 
     sendPrivateMessageRequest(userName: string = '', message: string = '') {
         if(userName == '' || message == '') {
@@ -141,22 +141,22 @@ export class ClientMessageHandler {
 
         this.socket.emit('pm', userName, message);
         return;
-    }
+    };
 
     sendAddFriendRequest(userName: string = '') {
         if(userName == '') {
             DisplayDriver.print('Invalid command /add \n');
             DisplayDriver.print('Usage: /add {user_name}\n');
             return;
-        }
+        };
 
         this.socket.emit('addFriend', userName);
         return;
-    }
+    };
 
     sendMessage(message: string) {
         this.socket.emit('msg', message);
         return;
-    }
+    };
 
-}
+};
