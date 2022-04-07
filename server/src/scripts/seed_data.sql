c38eb627001fd85530a4199910442edd53794a31
baudet_c <baudet_c@etna-alternance.net>
2022-04-07 10:23:04 +0200

    updating bdd scripts

--- server/src/scripts/seed_data.sql
+++ server/src/scripts/seed_data.sql
@@ -1,1 +1,2 @@
-INSERT INTO room VALUE ('Accueil'),('ETNA'),('Chill'),('Games');
+INSERT INTO room (name) VALUE ('Accueil'),('ETNA'),('Chill'),('Games');
+INSERT INTO user (username, password) VALUE ('Kijaru', '123'),('Melenchon', 'President.2022');
