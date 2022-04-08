c1d7721820d5048d321cec7293c2849172e05f3b
Nathan Debaz <nathandebaz266@gmail.com>
2022-04-08 14:14:39 +0200

    create user display

--- client/src/Core/core.ts
+++ client/src/Core/core.ts
@@ -25,61 +25,59 @@ export default class Core{
 
         this.clientMessageHandler = new ClientMessageHandler(this.connection.getSocket());
         this.serverMessageHandler = new ServerMessageHandler(
-                                        this.connection.getSocket(), 
+                                        this.connection.getSocket(),
                                         this.clientMessageHandler,
                                         this);
-
-
     };
 
-    public setServerBanner(banner: string) {
-        this.serverBanner = banner;
-        this.serverBannerSize = (banner.match(/\n/g) || '').length + 1;
-    }
+  public setServerBanner(banner: string) {
+    this.serverBanner = banner;
+    this.serverBannerSize = (banner.match(/\n/g) || '').length + 1;
+  }
 
-    public getServerBannerSize() {
-        return this.serverBannerSize;
-    }
+  public getServerBannerSize() {
+    return this.serverBannerSize;
+  }
 
-    public async startLoginPhase() {
-        DisplayDriver.print('\n')
-        this.clientMessageHandler.startLoginProcess();
-        this.clientMessageHandler.setPhase(Phase.login);
-    }
+  public async startLoginPhase() {
+    DisplayDriver.print('\n')
+    this.clientMessageHandler.startLoginProcess();
+    this.clientMessageHandler.setPhase(Phase.login);
+  }
 
-    public async startRoomsListPhase() {
-        DisplayDriver.clearTerminal();
-        DisplayDriver.print(this.serverBanner);
-        DisplayDriver.setCurrentPrompt('> ');
-        this.clientMessageHandler.sendRoomsListRequest();
-        this.clientMessageHandler.setPhase(Phase.roomList);
-    }
+  public async startRoomsListPhase() {
+    DisplayDriver.clearTerminal();
+    DisplayDriver.print(this.serverBanner);
+    DisplayDriver.setCurrentPrompt('> ');
+    this.clientMessageHandler.sendRoomsListRequest();
+    this.clientMessageHandler.setPhase(Phase.roomList);
+  }
 
-    public startChatPhase() {
-        DisplayDriver.clearTerminal();
-        this.clientMessageHandler.setPhase(Phase.chat);
-        DisplayDriver.startChat();
-    }
+  public startChatPhase() {
+    DisplayDriver.clearTerminal();
+    this.clientMessageHandler.setPhase(Phase.chat);
+    DisplayDriver.startChat();
+  }
 
-    consolePhase(arg: Phase) {
-        switch (arg) {
-            case Phase.load:
-                break;
+  consolePhase(arg: Phase) {
+    switch (arg) {
+      case Phase.load:
+        break;
 
-            case Phase.login:
-                this.startLoginPhase()
-                break;
+      case Phase.login:
+        this.startLoginPhase()
+        break;
 
-            case Phase.roomList:
-                this.startRoomsListPhase()
-                break;
+      case Phase.roomList:
+        this.startRoomsListPhase()
+        break;
 
-            case Phase.chat:
-                this.startChatPhase();
-                break;
+      case Phase.chat:
+        this.startChatPhase();
+        break;
 
-          default:
-              break;
-        };
+      default:
+        break;
     };
+  };
 };
